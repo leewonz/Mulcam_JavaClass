d7d56f989329e08403cb8ce860cf22d8e7f5c6fd
leewonz <lwk530@gmail.com>
2020-07-17 18:23:37 +0900

    Initial commit

---
+++ 1_java/src/day01/sample04.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class sample04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1+2+":"+3+4+5);
+		System.out.printf("%d ~ %d %n",Integer.MIN_VALUE, Integer.MAX_VALUE);
+		
+		int x = Integer.MAX_VALUE;
+		x++;
+		System.out.println(x);
+		
+		long y = Integer.MAX_VALUE;
+		y = 2147483648l;
+		y++;
+		System.out.println(y);
+	}
+
+}
